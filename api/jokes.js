c9aab96fdb25ba44dc2a93d629e5c3710fa35475
miqueet <5346303+miqueet@users.noreply.github.com>
2024-10-27 09:56:36 -0400

    Update jokes.js

--- api/jokes.js
+++ api/jokes.js
@@ -2,19 +2,11 @@ import fs from 'fs';
 import path from 'path';
 
 export default function handler(req, res) {
-    // Define the directory where jokes are stored
     const jokesDir = path.join(process.cwd(), 'jokes');
-    
-    // Read all filenames in the jokes directory
     const jokeFiles = fs.readdirSync(jokesDir);
-
-    // Randomly select a joke file
     const randomFile = jokeFiles[Math.floor(Math.random() * jokeFiles.length)];
-    
-    // Read the content of the randomly selected file
     const joke = fs.readFileSync(path.join(jokesDir, randomFile), 'utf8');
 
-    // Respond with the joke as plain text
     res.setHeader("Content-Type", "text/plain");
     res.status(200).send(joke);
 }
